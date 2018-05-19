a5a8c881850484f611081745636d732f6e9623d3
Henrik Heine <henrikheine@web.de>
2018-05-19 10:32:05 +0000

    apache http client added

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
 (defproject wstest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot wstest.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.apache.httpcomponents/httpclient "4.5.5"]
+                 #_ [swank-clojure/swank-clojure "1.4.3"]
+                 #_ [org.clojure/tools.nrepl "0.2.12"]]
+  
+  :plugins [[lein-swank "1.4.5"]]
+
+  ;; https://docs.oracle.com/javase/7/docs/technotes/guides/security/jsse/ReadDebug.html
+  :jvm-opts ["-D__javax.net.debug=all"]
+  
+  :main wstest.core)
--- src/wstest/core.clj
+++ src/wstest/core.clj
@@ -1,7 +1,41 @@
-(ns wstest.core
-  (:gen-class))
+(ns wstest.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn log [& xs]
+  (.println System/out (apply str xs)))
+
+;; https://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+
+(def head-size 200)
+
+(defn http-get-url [url-str]
+  (log "Trying '" url-str "'")
+  (let [url (java.net.URL. url-str)
+        con (.openConnection url)
+        _ (.setRequestProperty con "User-Agent" "Mozilla/5.0")
+        rc (.getResponseCode con)
+        _ (log "getResponseCode : " rc)]
+    (with-open [in (.getInputStream con)]
+      (log "Content: '" (.substring (slurp in) 0, head-size) "'"))))
+
+(defn http-get-httpclient [url-str]
+  (let [client (org.apache.http.impl.client.DefaultHttpClient.)
+        request (org.apache.http.client.methods.HttpGet. url-str)
+        _ (.addHeader request "User-Agent" "Mozilla/5.0")
+        response (.execute client request)
+        rc (-> response .getStatusLine .getStatusCode)
+        _ (log "getResponseCode : " rc)]
+    (with-open [in (-> response .getEntity .getContent)]
+      (log "Content: '" (.substring (slurp in) 0, head-size) "'"))))
+
+;; lein run url https://www.mopo.de/
+;; lein run apache 'https://www.google.com/search?q=foo'
+
+(defn -main [& [client url]]
+  (when-not url
+    (do
+      (log "Usage: wstest <client:url|apache> <url>")
+      (System/exit 1)))
+  (condp = client
+    "url" (http-get-url url)
+    "apache" (http-get-httpclient url)
+    (log "Unknown client '" client "'")))
